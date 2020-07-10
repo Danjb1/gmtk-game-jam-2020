4d8e15e1b0804702c4b1b6f4cb61ab3e7edbeb36
Mark Mizen <mark.c.mizen@gmail.com>
2020-07-10 22:47:35 +0100

    added pixi

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import * as PIXI from 'pixi.js';
+
+
+// Hello world
+let type = "WebGL"
+if (!PIXI.utils.isWebGLSupported()) {
+  type = "canvas"
+}
+
+PIXI.utils.sayHello(type)
+
+
+//Create a Pixi Application
+let app = new PIXI.Application({width: 1000, height: 1000});
+
+//Add the canvas that Pixi automatically created for you to the HTML document
+document.body.appendChild(app.view);
