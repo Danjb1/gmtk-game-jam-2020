133f373a14077273b26c28026680349a45f37b84
Dan Quill <admin@danjb.com>
2020-07-10 22:44:23 +0100

    Background colour

--- src/game.js
+++ src/game.js
@@ -16,7 +16,8 @@ export class Game {
     let canvas = document.getElementById('game-canvas');
     let ctx = canvas.getContext('2d');
     if (ctx) {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.fillStyle = 'rgb(255, 255, 255)';
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
       ctx.fillStyle = 'rgb(200, 0, 0)';
       ctx.fillRect(25, 25, 100, 100);
     }
