68aafff412095caa391bdc68452cbc076a2a0fd5
Dan Quill <admin@danjb.com>
2020-07-11 19:41:46 +0100

    Load image once

--- src/ui/pickup.component.ts
+++ src/ui/pickup.component.ts
@@ -1,5 +1,6 @@
 import { Game } from "../game/game";
 import { CatMetaComponent } from "../game/components";
+import { EntityContext } from '../game/entity-context';
 
 export class PickupComponent {
     
@@ -7,17 +8,17 @@ export class PickupComponent {
     public catMetaDataComponents = new Array<CatMetaComponent>();
     public catsOnScreen = new Array<CatMetaComponent>();
 
-    constructor(game: Game) {
-        setInterval(() => {
-            let catMetaDataComponents = game.getEntities()
-                .filter(entity => entity.getComponent(CatMetaComponent.KEY))
-                .map(entity => entity.getComponent(CatMetaComponent.KEY));
+    private image: HTMLImageElement;
+    private loaded = false;
 
-            this.catMetaDataComponents = (catMetaDataComponents as Array<CatMetaComponent>).filter(c => c.howCloseToPickup !== 1);
-            this.progress();
-        }, 10);
+    constructor(private context: EntityContext) {
+      this.image = new Image();
+      this.image.src = '../assets/images/player.png';
+      this.image.onload = () => {
+        this.loaded = true;
+      }
     }
-    
+
     public create(): HTMLCanvasElement {
         let canvas = document.createElement('pickupCanvas') as HTMLCanvasElement;
         canvas.innerHTML = '<canvas id="pickupCanvas" width="800" height="50" style="border:1px solid #d3d3d3; display: block; margin: auto;">';
@@ -26,7 +27,19 @@ export class PickupComponent {
         return canvas;
     }
 
-    public update() {}
+    public update() {
+
+      if (!this.loaded) {
+        return;
+      }
+
+      let catMetaDataComponents = this.context.getEntities()
+          .filter(entity => entity.getComponent(CatMetaComponent.KEY))
+          .map(entity => entity.getComponent(CatMetaComponent.KEY));
+
+      this.catMetaDataComponents = (catMetaDataComponents as Array<CatMetaComponent>).filter(c => c.howCloseToPickup !== 1);
+      this.progress();
+    }
 
     progress() {
         var canvas = document.getElementById("pickupCanvas") as HTMLCanvasElement;
@@ -46,13 +59,7 @@ export class PickupComponent {
             return;
         }
 
-        let image = new Image();
-        image.src = '../assets/images/player.png';
-        image.onload = () => {
-            let width = 800 * cat.howCloseToPickup;
-            context.drawImage(image, width, cat.value / 4);
-        };
-        
-        return image;
+        let width = 800 * cat.howCloseToPickup;
+        context.drawImage(this.image, width, cat.value / 4);
     }
 }
