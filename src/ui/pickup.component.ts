315f7c7cf37269331e994d452df555d62d5bd068
esummers1 <eddie.summers1@gmail.com>
2020-07-11 14:48:48 +0100

    Fix warnings

--- src/ui/pickup.component.ts
+++ src/ui/pickup.component.ts
@@ -1,32 +1,31 @@
 export class PickupComponent {
 
-    private canvas: HTMLCanvasElement;
+  private canvas: HTMLCanvasElement;
 
-    constructor() {
-        setInterval(() => this.moveAcrossTheScreen(), 10000);
-    }
+  constructor() {
+    setInterval(() => this.moveAcrossTheScreen(), 10000);
+  }
 
-    public create(): HTMLCanvasElement {
-        let canvas = document.createElement("pickupCanvas") as HTMLCanvasElement;
-        canvas.innerHTML = "<canvas id=\"pickupCanvas\" width=\"800\" height=\"50\" style=\"border:1px solid #d3d3d3; display: block; margin: auto;\">";
-        canvas.height = 50;
-        canvas.width = 800
-        return canvas;
-    }
+  public create(): HTMLCanvasElement {
+    let canvas = document.createElement('pickupCanvas') as HTMLCanvasElement;
+    canvas.innerHTML = '<canvas id="pickupCanvas" width="800" height="50" style="border:1px solid #d3d3d3; display: block; margin: auto;">';
+    canvas.height = 50;
+    canvas.width = 800;
+    return canvas;
+  }
 
-    moveAcrossTheScreen() {
-        let time = 30;
-        var canvas = document.getElementById("pickupCanvas") as HTMLCanvasElement;
-        var ctx = canvas.getContext("2d");
-        ctx.drawImage(this.insertImage(), 1, 2);
+  moveAcrossTheScreen() {
+    let time = 30;
+    var canvas = document.getElementById('pickupCanvas') as HTMLCanvasElement;
+    var ctx = canvas.getContext('2d');
+    ctx.drawImage(this.insertImage(), 1, 2);
 
-    }
+  }
 
-    insertImage(): CanvasImageSource {
-        let image = new Image();
-        image.src = '../assets/images/player.png';
-        return image;
-      }
-      
+  insertImage(): CanvasImageSource {
+    let image = new Image();
+    image.src = '../assets/images/player.png';
+    return image;
+  }
 
 }
