67477eba80783bbda0fa39225c35a822642420ef
esummers1 <eddie.summers1@gmail.com>
2020-07-11 11:26:39 +0100

    Rename factory

--- src/game/factory/cat.factory.ts
+++ src/game/factory/cat.factory.ts
@@ -2,7 +2,7 @@ import { Entity } from '../entity';
 import { HitboxComponent, WanderComponent } from '../components';
 import { CatMetaComponent } from '../components/cat-meta.component';
 
-export const createCatFactory = (): Entity => {
+export const createCat = (): Entity => {
   return new Entity()
     .attach(new HitboxComponent(0, 0, 10, 10))
     .attach(new CatMetaComponent())
