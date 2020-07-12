f702f40d4e766e03243c5aefd0c2419fd298712a
Dan Quill <admin@danjb.com>
2020-07-12 10:54:24 +0100

    Hide the fact that we are not picking up the "correct" cat

--- src/game/components/rescuer.component.ts
+++ src/game/components/rescuer.component.ts
@@ -2,9 +2,11 @@ import { Component } from '../component';
 import { CatMetaComponent } from './cat-meta.component';
 import { JailedComponent } from '.';
 import { Entity } from '../entity';
+import { HitboxComponent } from './hitbox.component';
+import { getHitboxFrom } from '../utils';
 
 /**
- * Handles a cat being picked up
+ * Component that picks up a Cat when due.
  */
 export class RescuerComponent extends Component {
   static readonly KEY = Symbol();
@@ -17,25 +19,19 @@ export class RescuerComponent extends Component {
     return !!e.getComponent(JailedComponent.KEY);
   }
 
-  update() {
+  public update() {
+    const cats = this.findAllCats();
 
-    // Figure out:
-    //  - How many cats are due to be picked up
-    //  - Which cats are eligible to be picked up
+    // Make lists of all the cats due / eligible for pickup
     const catsDueForPickup: Entity[] = [];
     const catsEligibleForPickup: Entity[] = [];
-    this.entity.context.getEntities().forEach(e => {
-      const catMeta = <CatMetaComponent> e.getComponent(CatMetaComponent.KEY);
-
-      if (!catMeta) {
-        // Entity is not a cat
-        return;
-      }
+    cats.forEach(e => {
 
       if (this._isInJail(e)) {
         catsEligibleForPickup.push(e);
       }
 
+      const catMeta = <CatMetaComponent> e.getComponent(CatMetaComponent.KEY);
       if (catMeta.howCloseToPickup >= 1) {
         catsDueForPickup.push(e);
       }
@@ -54,24 +50,58 @@ export class RescuerComponent extends Component {
       });
 
       if (suitableCat) {
-        this.pickUp(suitableCat);
+        this.pickUp(catDueForPickup, suitableCat);
       }
     });
   }
 
-  private pickUp(cat: Entity) {
+  private findAllCats(): Entity[] {
+    return this.entity.context.getEntities().filter(
+      e => (!!e.getComponent(CatMetaComponent.KEY))
+    );
+  }
 
-    const catMeta = <CatMetaComponent> cat.getComponent(CatMetaComponent.KEY);
+  private pickUp(catDueForPickup: Entity, catInPen: Entity) {
 
-    if (!catMeta.late) {
+    // Swap the 2 cats so that the one being picked up is in the pen!
+    // This disguises the fact that we are picking up any matching cat, not
+    // necessarily the exact cat that was due.
+    if (catDueForPickup !== catInPen) {
+      const h1: HitboxComponent = getHitboxFrom(catDueForPickup);
+      const h2: HitboxComponent = getHitboxFrom(catInPen);
+      this.swapHitboxes(h1, h2);
+    }
+
+    const catPickedUpMeta = <CatMetaComponent>
+        catDueForPickup.getComponent(CatMetaComponent.KEY);
+
+    if (!catPickedUpMeta.late) {
       // Add the cat's score to the game state
       this.entity.context
         .getState()
-        .increaseScore(catMeta.value);
+        .increaseScore(catPickedUpMeta.value);
     }
 
     // Delete the cat
-    cat.deleted = true;
+    catPickedUpMeta.deleted = true;
+  }
+
+  private swapHitboxes(h1: HitboxComponent, h2: HitboxComponent): void {
+
+    const x = h1.x;
+    const y = h1.y;
+    const speedX = h1.speedX;
+    const speedY = h1.speedY;
+
+    h1.x = h2.x;
+    h1.y = h2.y;
+    h1.speedX = h2.speedX;
+    h1.speedY = h2.speedY;
+
+    h2.x = x;
+    h2.y = y;
+    h2.speedX = speedX;
+    h2.speedY = speedY;
   }
 
 }
